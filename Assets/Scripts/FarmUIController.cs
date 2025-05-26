448d0d68237729fe96bc3aae8f89bd07fd6865cf
abbywang123 <166505049+abbywang123@users.noreply.github.com>
2025-05-26 17:33:39 +0800

    debug背包按鈕

--- Assets/Scripts/FarmUIController.cs
+++ Assets/Scripts/FarmUIController.cs
@@ -4,17 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class FarmUIController : MonoBehaviour
 {
-    public Button openInventoryButton; // 把按鈕拉進這裡
+    public Button openInventoryButton;
 
     void Start()
     {
+        Debug.Log("🟡 Start() 被執行了"); // 👉 加入這行確認 Start 是否有執行
         openInventoryButton.onClick.AddListener(OpenInventoryScene);
     }
 
     public void OpenInventoryScene()
     {
         Debug.Log("✅ 點擊成功：切換到背包場景");
-        SceneManager.LoadScene("Iventory");
+        SceneManager.LoadScene("Inventory");
     }
 }
+
 
